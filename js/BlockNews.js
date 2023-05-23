5d8b175a07eaf734328147f8b01065ca94410e24
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-05-24 02:31:00 +0400

    finish-demo

---
+++ js/BlockNews.js
@@ -0,0 +1,40 @@
+var news = [
+    {
+      image: "./img/slider1.svg",
+    },
+    {
+      image: "./img/slider2.svg",
+    },
+    {
+      image: "./img/slider3.svg",
+    }
+  ];
+  
+  var currentNewsIndex = 0;
+  
+  function updateNews() {
+    var newsBlock = document.querySelector(".slider__matches");
+    var newsImage = newsBlock.querySelector(".order_img");
+    var currentNews = news[currentNewsIndex];
+    newsImage.src = currentNews.image;
+    newsBlock.classList.add("visible");
+  }
+  
+  function rotateNews() {
+    currentNewsIndex++;
+    if (currentNewsIndex >= news.length) {
+      currentNewsIndex = 0;
+    }
+  
+    gsap.fromTo(
+      ".slider__matches",
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 0.3 }
+    );
+  
+    updateNews();
+  }
+  // блок с перезагрузки
+  updateNews();
+  // время между картинками массива
+  setInterval(rotateNews, 5000);
